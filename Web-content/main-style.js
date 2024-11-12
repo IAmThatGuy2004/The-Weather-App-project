fd53cfd171803878601d87817044429958d5b462
Li11z <Afoudali@hotmail.com>
2024-11-12 14:29:04 -0800

    search bar element

--- Web-content/main-style.js
+++ Web-content/main-style.js
@@ -1,10 +1,10 @@
 
 const backimage = [];
 
-backimage[0]='images/aurora-sky.jpg';
-backimage[1]='images/weather-rainbow.jpg';
-backimage[2]='images/snowy-nature-weather.jpg';
-backimage[3]='images/rainy-wallpaper.jpg';
+
+backimage[0]='images/weather-rainbow.jpg';
+backimage[1]='images/snowy-nature-weather.jpg';
+backimage[2]='images/rainy-wallpaper.jpg';
 
 window.onload  = function(){
     const random = Math.floor(Math.random() * backimage.length);
