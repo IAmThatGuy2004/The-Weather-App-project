d81b2804f59e9723d4d0fa3e73bd227fb9e196ae
Li11z <Afoudali@hotmail.com>
2024-11-16 04:32:45 -0800

    added function for emoji , returns appriate weather emoji depednign on weather type wheater type is differentiated by weather id retrieved from api

--- Web-content/main-style.js
+++ Web-content/main-style.js
@@ -75,23 +75,47 @@ async function getweatherdata() {
       tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
       humidityDisplay.textContent = `Humidity: ${humidity}%`;
       descDisplay.textContent = description;
-      
+      weatherEmoji.textContent = getWeatherEmoji(id);
   
       cityDisplay.classList.add("cityDisplay");
       tempDisplay.classList.add("tempDisplay");
       humidityDisplay.classList.add("humidityDisplay");
       descDisplay.classList.add("descDisplay");
-      
+      weatherEmoji.classList.add("weatherEmoji");
   
       geoloc.appendChild(cityDisplay);
       geoloc.appendChild(tempDisplay);
       geoloc.appendChild(humidityDisplay);
       geoloc.appendChild(descDisplay);
-      //card.appendChild(weatherEmoji);
+      geoloc.appendChild(weatherEmoji);
+      
 
 }
-
 getweatherdata(); // Ensure you call the function to initiate the request
+
+
+function getWeatherEmoji(weatherId){
+
+  switch(true){
+      case (weatherId >= 200 && weatherId < 300):
+          return "⛈";
+      case (weatherId >= 300 && weatherId < 400):
+          return "🌧";
+      case (weatherId >= 500 && weatherId < 600):
+          return "🌧";
+      case (weatherId >= 600 && weatherId < 700):
+          return "❄";
+      case (weatherId >= 700 && weatherId < 800):
+          return "🌫";
+      case (weatherId === 800):
+          return "☀";
+      case (weatherId >= 801 && weatherId < 810):
+          return "☁";
+      default:
+          return "❓";
+  }
+}
+
 
 
 //module.exports = { fetchCity };
