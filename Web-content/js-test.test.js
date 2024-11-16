b7a98956d352c24a71a9d958a0ce456ad3b5fb5a
Li11z <Afoudali@hotmail.com>
2024-11-16 00:04:43 -0800

    downloaded vtest, added test script to packages.json made a demo test that always passes make sure vitest works

---
+++ Web-content/js-test.test.js
@@ -0,0 +1,7 @@
+import { describe, test ,it, expect } from 'vitest';
+
+describe('Basic math test', () => {
+    it('should add 1 + 2 to equal 3', () => {
+      expect(1 + 2).toBe(3);  // This test will pass because 1 + 2 equals 3
+    });
+  });
