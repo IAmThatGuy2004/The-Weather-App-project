0cb42d0b8269ac2f56f4342da31edbdf05413f19
Li11z <Afoudali@hotmail.com>
2024-11-26 04:28:22 -0800

    set up of currentLocation function: uses native geolocation API which provided by browser, will extract longitude and latitude of selected location and then calls updateWeather function, on failure will just reset to default location
    searchedLocation: is function that accpet query string of lat and lon of a select city and splits info appropriatly , passes argument back to updateWeather

--- Web-content/weather-route.js
+++ Web-content/weather-route.js
@@ -4,12 +4,17 @@ import { updateWeather, error404} from "weather-app.js";
 const defaultLocation = "#/weather?lat=49.88307&lon=-119.48568"  //lat and lon of Kelowna, will later implement way to auto retrieve
 
 const currentLocation = function(){
+window.navigator.geolocation.getCurrentPosition(res => {
+    const{latitude, longitude}=res.coords;
 
+    updateWeather(`lat=${latitude}`,`lon=${longitude}`);
+}, err=>{
+    window.location.hash = defaultLocation;
+});
 }
 
-const searchedLocation = query =>{
-
-}
+const searchedLocation = query =>updateWeather(...query.split("&"));
+// updateWeather("lat=49", "lon=-119")
 
 const routes = new Map([
 
