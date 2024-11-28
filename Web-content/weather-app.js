6eadc2101b8cf52664d913406abe1e4f17ab4f79
Li11z <Afoudali@hotmail.com>
2024-11-28 04:47:48 -0800

    current weather dynamic display completed. shows the current temp and weather and date of each city searched for

--- Web-content/weather-app.js
+++ Web-content/weather-app.js
@@ -8,6 +8,10 @@ const searchView = document.querySelector("[data-search-view]");
 const searchResult = document.querySelector("[data-search-result]");
 const searchLocation = document.querySelectorAll("[data-search-location]");
 
+const currentweather = document.querySelector("[data-current-weather]");
+const fivedays = document.querySelectorAll("[data-5-day-forecast]");
+
+
 // Function to set default location based on user's current position
 function setDefaultLocation() {
   if ("geolocation" in navigator) {
@@ -115,6 +119,8 @@ searchField.addEventListener("input", function () {
         item.addEventListener("click", () => {
           searchView.classList.remove("active");
           searchResult.classList.remove("active");
+
+          
         });
       });
     });
@@ -306,9 +312,50 @@ async function displayLeft(coords) {
     console.log('Weather  retrieved:', weatherDetails);
     console.log('Location retrieved:', locationDetails);
 
+
+    const card = document.createElement("div");
+        card.classList.add("card", "card-lg", "current-weather-card");
+
+        card.innerHTML = `
+         
+              <h2 class="title-2 card-title">Now in:</h2>
+             
+
+
+              <div class="wrapper">
+                <p class="heading">${weatherDetails.temp}&deg;<sup>C</sup></p>
+
+                <img src="images/${weatherDetails.icon}.png" width="64" height="64" class="weather-icon">
+              </div>
+
+              <p class="body-3">${weatherDetails.description}</p>
+
+              <ul class="meta-list">
+                <li class="meta-item">
+                  <span class="m-icon"><i class="ri-calendar-event-line"></i></span>
+
+                  <p class="title-3 meta-text">${weatherDetails.date}</p>
+                </li>
+
+                <li class="meta-item">
+                  <span class="m-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 20.8995L16.9497 15.9497C19.6834 13.2161 19.6834 8.78392 16.9497 6.05025C14.2161 3.31658 9.78392 3.31658 7.05025 6.05025C4.31658 8.78392 4.31658 13.2161 7.05025 15.9497L12 20.8995ZM12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364L12 23.7279ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13ZM12 15C9.79086 15 8 13.2091 8 11C8 8.79086 9.79086 7 12 7C14.2091 7 16 8.79086 16 11C16 13.2091 14.2091 15 12 15Z"></path></svg></span>
+
+                  <p class="title-3 meta-text">${locationDetails.name} ${locationDetails.state}, ${locationDetails.country}</p>
+                </li>
+              </ul>
+        
+        `;
+
+        const currentweather = document.querySelector("[data-current-weather]");
+        currentweather.appendChild(card);
+
+
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
 
 displayLeft(coords);
+
+
+
