7404390e02093b4afd102f08a9685e7cff3f7ba3
Li11z <Afoudali@hotmail.com>
2024-11-28 12:01:12 -0800

    new api calls for hourly and daily forecast by different api since openweather doesnt offer it freely

--- Web-content/weather-api.js
+++ Web-content/weather-api.js
@@ -32,6 +32,18 @@ export const url = {
   },
 };
 
+export const openurl = {
+
+  hourly(lat,lon){
+    return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weather_code,wind_speed_10m`;
+  },
+  
+  daily(lat,lon){
+    return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min`;
+  },
+
+};
+
 //current-whether: https://api.openweathermap.org/data/2.5/weather?lat=23.7644025&lon=90.389015&units=metric&appid=your_api
 //current by city: https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${weatherkey}
 
@@ -51,3 +63,22 @@ export const url = {
 //geocoding-reverse:http://api.openweathermap.org/geo/1.0/reverse?lat=49.8879177&lon=-119.495902&limit=5&appid=6d7d8491e5e2c3a0173e9d4c2ff5ddbd
 
 //  https://api.openweathermap.org/data/2.5/forecast/hourly? lat=49.8879177&lon=-119.495902&appid=6d7d8491e5e2c3a0173e9d4c2ff5ddbd 
+
+
+/*
+WMO Weather interpretation codes (WW)
+Code	Description
+0	Clear sky
+1, 2, 3	Mainly clear, partly cloudy, and overcast
+45, 48	Fog and depositing rime fog
+51, 53, 55	Drizzle: Light, moderate, and dense intensity
+56, 57	Freezing Drizzle: Light and dense intensity
+61, 63, 65	Rain: Slight, moderate and heavy intensity
+66, 67	Freezing Rain: Light and heavy intensity
+71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+77	Snow grains
+80, 81, 82	Rain showers: Slight, moderate, and violent
+85, 86	Snow showers slight and heavy
+95 *	Thunderstorm: Slight or moderate
+96, 99 *	Thunderstorm with slight and heavy hail
+*/
