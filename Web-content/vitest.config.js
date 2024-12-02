f11f6625bca8213df6a38094173c39a3cfcde434
MSamarasekara <maddysam60@gmail.com>
2024-12-01 22:02:56 -0800

    deleting duplicate folders

--- Web-content/vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    globals: true, // allows for global variables like document, window, etc.
-    environment: 'jsdom', // ensures jsdom is used as the environment
-  },
-});
